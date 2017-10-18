e5b8f88f80f6534b3fe6b0e456be22c704928c4f
Jens Lindhardt <jens@lindhardt-freelance.dk>
2017-10-18 12:02:22 +0200

    Start demo module 5

--- Vitter/Models/Video.cs
+++ Vitter/Models/Video.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,6 +10,8 @@ namespace Vitter.Models
     public class Video
     {
         public int VideoID { get; set; }
+
+        [Required]
         public string Title { get; set; }
         public string Description { get; set; }
 
